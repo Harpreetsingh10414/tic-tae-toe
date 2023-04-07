5e4de25a37cdcff5bf6d83183d46ea075eaca32c
Harpreet Singh <harpreetsingh10414@gmail.com>
2023-04-07 20:37:05 +0530

    Basic Structure with player score variable added

--- tic_tae_toe/lib/main.dart
+++ tic_tae_toe/lib/main.dart
@@ -7,109 +7,195 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _HomePageState extends State<HomePage> {
+  bool onTrue = true; //the first player is 0
+  List<String> displayExoh = ['', '', '', '', '', '', '', '', ''];
+  var myTextStyle = TextStyle(color: Colors.white, fontSize: 30);
+  int ohScore = 0;
+  int exScore = 0;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      backgroundColor: Colors.grey.shade800,
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+                child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(30.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Player O',
+                        style: myTextStyle,
+                      ),
+                      Text(
+                        ohScore.toString(),
+                        style: myTextStyle,
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(30.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Player X',
+                        style: myTextStyle,
+                      ),
+                      Text(
+                        exScore.toString(),
+                        style: myTextStyle,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            )),
+          ),
+          Expanded(
+            flex: 3,
+            child: GridView.builder(
+              itemCount: 9,
+              gridDelegate:
+                  SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+              itemBuilder: (BuildContext context, int index) {
+                return GestureDetector(
+                  onTap: () {
+                    _tapped(index);
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey.shade700),
+                    ),
+                    child: Center(
+                      child: Text(
+                        displayExoh[index],
+                        style: TextStyle(color: Colors.white, fontSize: 40),
+                      ),
+                    ),
+                  ),
+                );
+              },
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          Expanded(
+            child: Container(
+        
             ),
-          ],
-        ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  void _tapped(int index) {
+    setState(() {
+      if (onTrue && displayExoh[index] == '') {
+        displayExoh[index] = 'O';
+      } else if (!onTrue && displayExoh[index] == '') {
+        displayExoh[index] = 'X';
+      }
+
+      onTrue = !onTrue;
+      _checkWinner();
+    });
+  }
+
+  void _checkWinner() {
+    //checks for 1st row
+    if (displayExoh[0] == displayExoh[1] &&
+        displayExoh[0] == displayExoh[2] &&
+        displayExoh[0] != '') {
+      _showWinDialog(displayExoh[0]);
+    }
+
+    //checks for 2nd row
+    if (displayExoh[3] == displayExoh[4] &&
+        displayExoh[3] == displayExoh[5] &&
+        displayExoh[3] != '') {
+      _showWinDialog(displayExoh[3]);
+    }
+
+    //checks for 3rd row
+    if (displayExoh[6] == displayExoh[7] &&
+        displayExoh[6] == displayExoh[8] &&
+        displayExoh[6] != '') {
+      _showWinDialog(displayExoh[6]);
+    }
+
+    //checks for 1st column
+    if (displayExoh[0] == displayExoh[3] &&
+        displayExoh[0] == displayExoh[6] &&
+        displayExoh[0] != '') {
+      _showWinDialog(displayExoh[0]);
+    }
+
+    //checks for 2nd column
+    if (displayExoh[1] == displayExoh[4] &&
+        displayExoh[1] == displayExoh[7] &&
+        displayExoh[1] != '') {
+      _showWinDialog(displayExoh[1]);
+    }
+
+    //checks for 3rd column
+    if (displayExoh[2] == displayExoh[5] &&
+        displayExoh[2] == displayExoh[8] &&
+        displayExoh[2] != '') {
+      _showWinDialog(displayExoh[2]);
+    }
+
+    //checks diagonal
+    if (displayExoh[6] == displayExoh[4] &&
+        displayExoh[6] == displayExoh[2] &&
+        displayExoh[6] != '') {
+      _showWinDialog(displayExoh[6]);
+    }
+
+    //checks diagonal
+    if (displayExoh[0] == displayExoh[4] &&
+        displayExoh[0] == displayExoh[8] &&
+        displayExoh[0] != '') {
+      _showWinDialog(displayExoh[0]);
+    }
+  }
+
+  void _showWinDialog(String winner) {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('WINNER IS: ' + winner),
+          );
+        });
+
+    if (winner == 'O') {
+      ohScore += 1;
+    } else if (winner == 'X') {
+      exScore += 1;
+    }
   }
 }
